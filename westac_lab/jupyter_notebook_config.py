84300d76a1fe4670119d210bc6b90e0ce0501afc
Roger Mähler <roger.mahler@hotmail.com>
2021-05-01 13:00:34 +0200

    Use env variable

--- westac_lab/jupyter_notebook_config.py
+++ westac_lab/jupyter_notebook_config.py
@@ -1,6 +1,8 @@
+import os
+
 c = get_config()
 
 c.NotebookApp.ip = u'0.0.0.0'
-c.NotebookApp.port = 8888
+c.NotebookApp.port = os.environ.get('NB_PORT', 8888)
 c.NotebookApp.open_browser = False
 c.FileContentsManager.delete_to_trash = False
