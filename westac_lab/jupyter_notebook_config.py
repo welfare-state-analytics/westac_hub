17aa3a95ed78dc73c060df91b042fd359e225876
Roger Mähler <roger.mahler@hotmail.com>
2021-05-02 21:10:42 +0200

    FIXME

--- westac_lab/jupyter_notebook_config.py
+++ westac_lab/jupyter_notebook_config.py
@@ -1,8 +1,9 @@
+# FIXME #10 Deprecate this file since it's not used
 import os
 
 c = get_config()
 
 c.NotebookApp.ip = u'0.0.0.0'
-c.NotebookApp.port = os.environ.get('NB_PORT', 8888)
+c.NotebookApp.port = os.environ.get('${LAB_PORT}', 8888)
 c.NotebookApp.open_browser = False
 c.FileContentsManager.delete_to_trash = False
